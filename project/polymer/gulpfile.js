24d10c71f13a27d95f6d7c8c92bcdb3c215ec409
cameronwp <cameron.w.pittman@gmail.com>
2015-10-12 21:41:24 -0700

    adds final project solution with polymer

---
+++ project/polymer/gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+
+gulp.task('vulcanize', function () {
+	return gulp.src('src/create.html')
+		.pipe(vulcanize({
+			abspath: '',
+			excludes: [],
+			stripExcludes: false
+		}))
+		.pipe(gulp.dest('dest', {basename: 'create.html'}));
+});
+
+gulp.task('compress', function() {
+	return gulp.src('src/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dest/js'));
+});
+
+gulp.task('minify-css', function() {
+	return gulp.src('src/css/*.css')
+		.pipe(minifyCss({compatibility: 'ie8'}))
+		.pipe(gulp.dest('dest/css'));
+});
+
+gulp.task('default', ['vulcanize', 'compress', 'minify-css']);
+
+var files = [
+	'src/*',
+	'src/*/*'
+];
+
+gulp.task('watch', function () {
+	watch(files, batch(function (events, done) {
+		gulp.start('default', done);
+	}));
+});
---
+++ project/polymer/src/js/app.js
@@ -0,0 +1,34 @@
+var useDiffBilling = document.querySelector('.use-diff-billing');
+var newBilling = document.querySelector('.new-billing');
+var submit = document.querySelector('.submit');
+var scroller = document.querySelector('paper-header-panel').scroller;
+
+var currScrollTop, endScrollTop, diff;
+function scrollDown () {
+	scroller.scrollTop = scroller.scrollTop + diff/12;
+
+	if (Math.abs(scroller.scrollTop - endScrollTop) > 10) {
+		window.requestAnimationFrame(scrollDown);
+	}
+};
+
+function scrollUp () {
+	scroller.scrollTop = scroller.scrollTop + diff/12;
+
+	console.log(scroller.scrollTop, endScrollTop);
+
+	if (Math.abs(scroller.scrollTop - endScrollTop) > 10) {
+		window.requestAnimationFrame(scrollDown);
+	}
+};
+
+useDiffBilling.addEventListener('change', function (e) {
+	newBilling.classList.toggle('fade-in');
+
+	if (this.checked) {
+		currScrollTop = scroller.scrollTop;
+		endScrollTop = submit.getBoundingClientRect().bottom;
+		diff = endScrollTop - currScrollTop;
+		window.requestAnimationFrame(scrollDown);	
+	}
+});
